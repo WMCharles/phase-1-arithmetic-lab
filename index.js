f69f18446017604843e530e277b7d8916fd795bd
WMCharles <charles.wafula@student.moringaschool.com>
2022-08-22 17:41:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let multiply, num1, num2;
+num1 = 1;
+num2 = 62
+multiply = num1*num2
+
+let random = Math.floor((Math.random()*10))+1
+
+let mod, num3, num4;
+num3=15;
+num4=11;
+mod = num3%num4
+
+let max = Math.max(1,2,20)
